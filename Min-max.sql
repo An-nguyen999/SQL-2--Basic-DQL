7133c7e9a763e4b7274af278b3b6983d10c6e746
An (Selina) Nguyen <163428523+An-nguyen999@users.noreply.github.com>
2024-04-24 15:19:05 +0700

    Add files via upload

---
+++ Min-max.sql
@@ -0,0 +1,10 @@
+select min(UnitPrice) as Min_price
+from Products
+
+
+select max(orderdate) as Ngay_dat_hang_gan_nhat
+from Orders
+
+
+select max([UnitsInStock]) as max_UnitsInStock
+from Products
---
+++ Select-top-percent.sql
@@ -0,0 +1,13 @@
+﻿select top 5*
+from Customers
+
+-- lấy ra 30% nhân viên của công ty hiện tại
+select top 30 percent * -- * là lấy hết các cột
+from Employees
+
+select top 30 percent FirstName, LastName, Title -- * là lấy hết các cột
+from Employees
+
+-- lấy giá trị khác nhau và chỉ 5 dòng đầu tiên
+select distinct top 5 CustomerID
+from Orders
